f797318b88e27fdd9dd16ec6b6c53ac3968c705b
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-17 18:21:18 +0530

    refactor: move headerNavigation to constants and update Header component styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,14 @@
+import { headerNavigation } from "../constants/constants";
+
 const Header = () => {
-  const headerNavigation = [
-    { label: "Home", url: "#" },
-    { label: "About", url: "#" },
-    { label: "Products", url: "#" },
-    { label: "Enquiry", url: "#" },
-  ];
   return (
     <div className="flex justify-between items-center w-full px-20 py-4 text-sm  border-b-[0.2px] border-neutral-400">
-      <h1 className="capitalize text-lg">aarcane internaltional</h1>
+      <h1 className="capitalize text-2xl font-instrumentRegular">
+        aarcane{" "}
+        <span className="font-instrumentItalic text-base capitalize">
+          internaltional
+        </span>
+      </h1>
       <div>
         <h1 className="flex justify-center items-center gap-10">
           {headerNavigation.map((i, index) => (
