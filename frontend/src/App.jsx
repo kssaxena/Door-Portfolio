6a3f10ade01688c3ad3f218e2119f0799c48bcf0
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-18 10:30:35 +0530

    feat: add Footer component to App for improved layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { IoCall } from "react-icons/io5";
 import { IoMdMail } from "react-icons/io";
 import AppRoutes from "./routes/Routes";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="bg-[#F6F2EA] text-neutral-700 font-dosis text-[20px]">
       <Header />
       <AppRoutes />
+      <Footer />
     </div>
   );
 }
