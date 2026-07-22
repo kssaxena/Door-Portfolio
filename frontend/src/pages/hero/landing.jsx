23ebf27d818f0d1d0f324fe1128502898329064a
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-22 20:54:22 +0530

    feat: refactor Landing component to use ButtonWrapper and improve layout structure

--- frontend/src/pages/hero/landing.jsx
+++ frontend/src/pages/hero/landing.jsx
@@ -1,16 +1,16 @@
 import { motion } from "framer-motion";
 import { useParallax } from "../../components/hooks/ParallaxImage";
 import { useRef } from "react";
-import Button from "../../components/Button";
+import ButtonWrapper from "../../components/Button";
 
 const Landing = () => {
   const imageRef = useRef(null);
   const { y, scale } = useParallax(imageRef);
   return (
-    <div className="flex justify-between items-center w-full px-40 py-20 relative">
-      <p className="absolute top-20 left-20 text-xs uppercase ">obs 01</p>
-      <div className="w-[40%] flex flex-col justify-start items-start gap-10">
-        <h1 className="text-[80px] leading-20 font-instrumentRegular tracking-tighter font-extralight">
+    <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center w-full lg:px-40 py-20 relative">
+      {/* <p className="absolute top-20 left-20 text-xs uppercase ">obs 01</p> */}
+      <div className="w-full lg:w-[40%] flex flex-col justify-start items-start gap-10">
+        <h1 className="text-[46px] lg:text-[80px] leading-10 lg:leading-20 font-instrumentRegular tracking-tighter font-extralight">
           Doors that define the first impression{" "}
           <span className="font-instrumentItalic text-[#6A4F3B]">
             impression
@@ -18,20 +18,20 @@ const Landing = () => {
           .
         </h1>
         <div className="border-[0.5px] w-10" />
-        <h1>
+        <h1 className="text-[16px] lg:text-[17px]">
           Every entrance is the beginning of a story. Crafted with precision
           engineering and refined detailing, Aarcane Doors combine security,
           durability, and timeless design for homes that deserve an
           extraordinary welcome.
         </h1>
-        <Button label={"Explore Collection"} />
+        <ButtonWrapper label={"Explore Collection"} />
       </div>
       <div
-        className="w-[60%]"
+        className="w-full lg:w-[60%]"
         ref={imageRef}
         className="overflow-hidden rounded-sm"
       >
-        <div className="w-[650px] h-[500px]">
+        <div className="w-full lg:w-[650px] h-full lg:h-[500px]">
           <motion.img
             style={{ y, scale }}
             className="w-full h-full object-cover"
