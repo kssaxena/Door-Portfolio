03f82a7f8c914012d4f1cdbfc11486ac8b7a751f
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-24 19:21:12 +0530

    feat: improve layout and responsiveness of ProductCard and DoorTypes components

--- frontend/src/components/ui/ProductCard.jsx
+++ frontend/src/components/ui/ProductCard.jsx
@@ -6,18 +6,18 @@ const ProductCard = ({ data = mappingData }) => {
   return data.map((d, index) => (
     <div
       key={index}
-      className={`h-100 w-[80vw] border-[0.2px] border-neutral-300 rounded overflow-hidden flex justify-center items-center ${index % 2 === 0 ? "flex-row" : "flex-row-reverse"}`}
+      className={`h-fit lg:h-100 md:w-[80vw] w-full border-[0.2px] border-neutral-300 rounded overflow-hidden flex flex-col justify-center items-center ${index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"}`}
     >
-      <div className="w-[50%] h-full">
+      <div className="w-full md:w-[50%] h-full">
         <img src={d.image} className="object-fill h-full w-full" />
       </div>
-      <div className="w-[50%] h-full px-10 flex flex-col justify-center items-start">
-        <div className="h-[90%] px-10 flex flex-col justify-between items-start">
+      <div className="w-full md:w-[50%] h-full md:px-10 flex flex-col justify-center items-start">
+        <div className="h-[90%] lg:px-10 flex flex-col justify-between items-start gap-5 md:gap-0 py-10 px-2">
           <p className="text-sm">
             <span>0{index + 1} / </span>
             {d.heading}
           </p>
-          <h1 className="text-[40px] leading-10 font-instrumentRegular tracking-tighter font-extralight">
+          <h1 className="text-[26px] md:text-[40px] leading-10 font-instrumentRegular tracking-tighter font-extralight">
             {d.quotation}
           </h1>
           <p className="text-[18px]">{d.description}</p>
@@ -31,17 +31,17 @@ const ProductCard = ({ data = mappingData }) => {
 
 const MaterialStudy = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full px-40 py-20 gap-5 relative">
+    <div className="flex flex-col justify-center items-center w-full px-5 md:px-40 py-20 gap-5 relative">
       <p className="absolute top-20 left-20 text-xs uppercase ">
         MATERIAL STUDY
       </p>
-      <div className="flex justify-between items-start gap-60 py-10">
-        <h1 className="text-[56px] leading-12 font-instrumentRegular tracking-tighter font-extralight w-[60%]">
+      <div className="flex flex-col lg:flex-row justify-between items-start md:gap-60 py-10">
+        <h1 className="text-[36px] lg:text-[56px] leading-10 lg:leading-12 font-instrumentRegular tracking-tighter font-extralight lg::w-[60%] w-full">
           Materials chosen for{" "}
           <span className="font-instrumentItalic text-[#6A4F3B]">strength</span>{" "}
           crafted for generations.
         </h1>
-        <p className="w-[40%] text-right">
+        <p className="w-[90%] lg:w-[40%] text-left lg:text-right">
           Engineered steel, premium finishes, advanced coatings, and precision
           manufacturing combine to create doors that resist corrosion, termites,
           moisture, and daily wear while maintaining exceptional beauty.{" "}
--- frontend/src/pages/hero/doorTypes.jsx
+++ frontend/src/pages/hero/doorTypes.jsx
@@ -2,10 +2,10 @@ import { data } from "../../constants/constants";
 
 const DoorTypes = () => {
   return (
-    <div className="px-40 py-20 relative">
-      <p className="absolute top-20 left-20 text-xs ">POSITION</p>
+    <div className="px-5 md:px-40 py-20 relative">
+      <p className="absolute top-20 right-10 md:left-20 text-xs ">POSITION</p>
       <div className="flex flex-col justify-start items-start gap-10">
-        <h1 className="text-[80px] leading-20 font-instrumentRegular tracking-tighter font-extralight w-[60vw]">
+        <h1 className="text-[46px] lg:text-[80px] leading-10 md:leading-20 font-instrumentRegular tracking-tighter font-extralight w-[60vw]">
           We believe every entrance should reflect,{" "}
           {/* <span className="font-instrumentItalic text-[#6A4F3B]">kept,</span> */}
           confidence{" "}
@@ -18,13 +18,13 @@ const DoorTypes = () => {
           </span>{" "}
         </h1>
         <div className="border-[0.5px] w-10" />
-        <div className="flex justify-start items-start gap-20 ">
-          <p className="w-72 leading-6">
+        <div className="flex flex-col md:flex-row justify-start items-start gap-5 md:gap-20 ">
+          <p className="w-full md:w-72 leading-6">
             Every Aarcane door is engineered to withstand changing climates
             while preserving its finish and structural integrity. Built for
             modern lifestyles without compromising elegance.
           </p>
-          <p className="w-72 leading-6">
+          <p className="w-full md:w-72 leading-6">
             From grand villa entrances to contemporary residences, our
             collections are designed to complement architecture rather than
             compete with it.
