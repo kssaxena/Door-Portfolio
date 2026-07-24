3892314423d02ae489bfeb3efcb897ba57a62fe2
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-24 19:21:27 +0530

    feat: enhance layout and responsiveness of Observation component

--- frontend/src/pages/hero/observation.jsx
+++ frontend/src/pages/hero/observation.jsx
@@ -4,11 +4,11 @@ import { mappingData3 } from "../../constants/constants";
 const Observation = () => {
   const Card = () => {
     return (
-      <div className="flex justify-center items-center gap-5 h-full">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-5 h-full">
         {mappingData3.map((data, index) => (
           <div
             key={index}
-            className={`flex flex-col justify-start items-start border-[0.2px] border-neutral-800 gap-5 w-fit h-[70vh]`}
+            className={`flex flex-col justify-start items-start border-[0.2px] border-neutral-800 gap-5 w-fit h-[60vh] md:h-[70vh]`}
           >
             <div className="overflow-hidden justify-center items-center flex h-[50%] w-full">
               <div className="w-full h-full">
@@ -30,9 +30,11 @@ const Observation = () => {
     );
   };
   return (
-    <div className="px-40 py-20 relative">
-      <p className="absolute top-20 left-20 text-xs uppercase">studio notes</p>
-      <div className="flex justify-between items-start gap-10">
+    <div className="px-5 lg:px-40 py-20 relative">
+      <p className="absolute top-20 right-10 lg:left-20 text-xs uppercase">
+        studio notes
+      </p>
+      <div className="flex flex-col lg:flex-row justify-between items-start gap-10">
         <h1 className="text-[54px] leading-12 font-instrumentRegular tracking-tighter font-extralight flex flex-col ">
           <span className="font-instrumentItalic text-[#6A4F3B]">
             Observations
@@ -40,8 +42,8 @@ const Observation = () => {
           gathered through material, light, and use.
         </h1>
         {/* <div className="border-[0.5px] w-10" /> */}
-        <div className="flex justify-start items-start gap-20 ">
-          <p className="w-72 leading-6 text-right">
+        <div className="w-full">
+          <p className="w-full md:w-72 leading-6 text-right">
             Explore handcrafted collections designed for modern homes, luxury
             villas, architects, and interior designers.
           </p>
