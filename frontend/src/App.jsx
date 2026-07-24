89e39bea0c09a72227a6ec5b7bb3ff09f634a7a5
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-24 19:37:10 +0530

    feat: add ScrollToTop component to improve user navigation experience

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { IoMdMail } from "react-icons/io";
 import AppRoutes from "./routes/Routes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/hooks/ScrollToTop";
 
 function App() {
   return (
     <div className="bg-[#F6F2EA] text-neutral-700 font-dosis text-[20px] selection:bg-neutral-500 selection:text-neutral-200">
       <Header />
+      <ScrollToTop />
       <AppRoutes />
       <Footer />
     </div>
