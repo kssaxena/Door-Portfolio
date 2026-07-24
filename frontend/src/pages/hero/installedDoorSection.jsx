7673eb0fbbdb0676084aa601471cfd01903b2c6b
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-24 19:21:33 +0530

    feat: enhance layout and responsiveness of InstalledDoorSection and Process components

--- frontend/src/pages/hero/installedDoorSection.jsx
+++ frontend/src/pages/hero/installedDoorSection.jsx
@@ -14,9 +14,10 @@ const InstalledDoorSection = () => {
         {mappingData2.map((data, index) => (
           <div
             key={index}
-            className={`flex justify-center items-center border-[0.2px] border-neutral-800 px-10 py-5 gap-5 ${index % 2 ? "flex-row" : "flex-row-reverse"}`}
+            className={`flex flex-col-reverse lg:flex-row-reverse justify-center items-center border-[0.2px] border-neutral-800 px-2 lg:px-10 py-5 gap-5`}
+            // className={`flex justify-center items-center border-[0.2px] border-neutral-800 px-2 lg:px-10 py-5 gap-5 ${index % 2 ? "lg:flex-row flex-col" : "flex-row-reverse"}`}
           >
-            <div className="w-[30%] flex flex-col justify-start items-start gap-10 h-full">
+            <div className="w-full lg:w-[30%] flex flex-col justify-start items-start gap-10 h-full">
               <p className="text-xs uppercase ">{data.heading}</p>
               <h1 className="text-[40px] leading-10 font-instrumentRegular tracking-tighter font-extralight">
                 {data.quotation}
@@ -26,7 +27,7 @@ const InstalledDoorSection = () => {
               <ButtonWrapper label={"view study"} />
             </div>
             <div
-              className="w-[70%] h-full overflow-hidden rounded-sm bg-red-400 justify-center items-center flex"
+              className="w-full lg:w-[70%] h-full overflow-hidden rounded-sm bg-red-400 justify-center items-center flex"
               //   ref={imageRef}
             >
               <div className="w-full h-full">
@@ -44,8 +45,8 @@ const InstalledDoorSection = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-end w-fit px-40 py-20 gap-10 bg-[#26211C] text-[#F6F2EA]">
-      <h1 className="text-[56px] leading-12 font-instrumentRegular tracking-tighter font-extralight indent-8">
+    <div className="flex flex-col justify-center items-end w-fit px-5 lg:px-40 py-20 gap-5 lg:gap-10 bg-[#26211C] text-[#F6F2EA]">
+      <h1 className="text-[36px] lg:text-[56px] leading-10 lg:leading-12 font-instrumentRegular tracking-tighter font-extralight indent-8">
         Collections engineered for{" "}
         <span className="font-instrumentItalic text-[#6A4F3B]">
           security, elegance,
--- frontend/src/pages/hero/process.jsx
+++ frontend/src/pages/hero/process.jsx
@@ -4,11 +4,11 @@ import { mappingData4 } from "../../constants/constants";
 const Process = () => {
   const Card = () => {
     return (
-      <div className="grid grid-cols-2 justify-center items-center h-full">
+      <div className="grid lg:grid-cols-2 justify-center items-center h-full w-full">
         {mappingData4.map((data, index) => (
           <div
             key={index}
-            className={`flex flex-col justify-start items-start border-[0.1px] border-neutral-300 gap-5 h-[35vh] w-[25vw] relative`}
+            className={`flex flex-col justify-start items-start border-[0.1px] border-neutral-300 gap-5 h-[20vh] lg:h-[35vh] w-full lg:w-[25vw] relative`}
           >
             <div className="flex flex-col h-full justify-end items-start gap-5 px-5 py-5">
               <p className="text-xs uppercase absolute top-5 left-5">
@@ -27,18 +27,20 @@ const Process = () => {
     );
   };
   return (
-    <div className="px-40 py-20 relative">
-      <p className="absolute top-20 left-20 text-xs uppercase">process</p>
-      <div className="flex justify-between items-start h-[70vh]">
-        <div className="flex flex-col justify-between items-start gap-10 h-full">
-          <h1 className="text-[54px] leading-12 font-instrumentRegular tracking-tighter font-extralight flex flex-col ">
+    <div className="px-5 lg:px-40 py-20 relative">
+      <p className="absolute top-20 right-10 lg:left-20 text-xs uppercase">
+        process
+      </p>
+      <div className="flex flex-col lg:flex-row justify-between items-start h-fit lg:h-[70vh]">
+        <div className="flex flex-col justify-between items-start gap-10 h-full w-full">
+          <h1 className="text-[36px] lg:text-[54px] leading-10 lg:leading-12 font-instrumentRegular tracking-tighter font-extralight flex flex-col ">
             A restrained{" "}
             <span className="font-instrumentItalic text-[#6A4F3B]">
               sequence
             </span>{" "}
             for considered work.
           </h1>
-          <div className="flex justify-start items-center gap-5 text-sm">
+          <div className="hidden lg:flex justify-start items-center gap-5 text-sm">
             <div className="border-[0.5px] h-20" />
             <div className="flex flex-col gap-3">
               <p>Field Note</p>
@@ -48,7 +50,7 @@ const Process = () => {
             </div>
           </div>
         </div>
-        <div>
+        <div className="w-full">
           <Card />
         </div>
       </div>
