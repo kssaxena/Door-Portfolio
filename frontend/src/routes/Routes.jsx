5d9de67c076f96145b48243a17a96cb23d4616bc
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-08-08 08:41:44 +0530

    feat: add About route to AppRoutes for improved navigation

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { lazy } from "react";
 
 const Hero = lazy(() => import("../pages/hero/page"));
+const About = lazy(() => import("../pages/about/page"));
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
+      <Route path="/about/aarcane-doors" element={<About />} />
     </Routes>
   );
 };
