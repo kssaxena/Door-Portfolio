ee0314184e0aad05f1f9b0bc87da5c07d74cdb73
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-09-23 20:13:38 +0530

    feat: implement page transition animations and context for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,50 @@ import AppRoutes from "./routes/Routes";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/hooks/ScrollToTop";
+import { createContext, useState } from "react";
+import PageTransition from "./components/ui/loader";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const TransitionContext = createContext(null);
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [showOverlay, setShowOverlay] = useState(false);
+  const [isExiting, setIsExiting] = useState(false);
+
+  const transitionTo = (path) => {
+    if (path === location.pathname) return;
+
+    setShowOverlay(true);
+    setIsExiting(false);
+
+    // Animate IN
+    setTimeout(() => {
+      navigate(path);
+    }, 700);
+
+    // Animate OUT
+    setTimeout(() => {
+      setIsExiting(true);
+    }, 700);
+
+    // Cleanup
+    setTimeout(() => {
+      setShowOverlay(false);
+    }, 1400);
+  };
+
   return (
-    <div className="bg-[#F6F2EA] text-neutral-700 font-dosis text-[20px] selection:bg-neutral-500 selection:text-neutral-200">
-      <Header />
-      <ScrollToTop />
-      <AppRoutes />
-      <Footer />
-    </div>
+    <TransitionContext.Provider value={transitionTo}>
+      <div className="bg-[#F6F2EA] text-neutral-700 font-dosis text-[20px] selection:bg-neutral-500 selection:text-neutral-200">
+        <Header />
+        <ScrollToTop />
+        {showOverlay && <PageTransition isExiting={isExiting} />}
+        <AppRoutes />
+        <Footer />
+      </div>
+    </TransitionContext.Provider>
   );
 }
 
